2597fb980da61c46177ecde88b939f78b02fdb4a
apotheosisss <164839152+apotheosisss@users.noreply.github.com>
2024-04-16 12:15:16 -0400

    Add files via upload

---
+++ curos = 10.py
@@ -0,0 +1,31 @@
+curso = 10
+
+curso = 20
+print(curso)
+
+# Cambiar el tipo de dato
+
+numerillo1= int(input("ingrese un número"))
+numerillo2= int(input("ingrese un numero"))
+suma= numerillo1+numerillo2
+print(suma)
+print(type(numerillo1))
+
+"""
+Ejercicio 1 convertir la temperatura de farenheit a celcius
+
+Ejercicio 2 calcular la edad exacta del usuario y consular al usuario si ya estuvo de cumpleaños este año o no.
+"""
+#Ejercicio 1
+fahrenheit = float(input("ingrese la temperatura en fahrenheit"))
+celsius = (fahrenheit -32) * 5/9
+celsius = (print("Los grados centigrados son:",celsius ))
+#Ejercicio 2
+cumple_este_año = input("Ha tenido su cumple este año?")
+if cumple_este_año = True
+añoDeNacimiento = int(input("Ingrese su año de nacimiento."))
+edad = 2024 - añoDeNacimiento
+print("Su edad es:",edad)
+if cumple_este_año = False
+edadnot = 2023 - añoDeNacimiento
+print("Su edad es: ",edadnot)
