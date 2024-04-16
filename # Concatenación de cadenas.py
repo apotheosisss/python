ae963481c466f4b76f6fcdb756282949a8690715
apotheosisss <164839152+apotheosisss@users.noreply.github.com>
2024-04-16 11:28:57 -0400

    Add files via upload

---
+++ # Concatenación de cadenas.py
@@ -0,0 +1,14 @@
+# Concatenación de cadenas
+saludo = "Hola"
+nombreAlumno = "Pedro"
+mensaje = saludo + " " + nombreAlumno + " Cómo estás"
+print(mensaje)
+edad = int
+es_estudiante = True
+tiene_trabajo = False
+# Operaciones lógicas con booleanos
+es_mayor_de_edad = edad >= 18 #comprobando si la edad es mayor o igual a 18
+puede_votar = es_mayor_de_edad and tiene_trabajo
+
+print("es mayor de edad? "), es_mayor_de_edad
+print("Puede votar? "), puede_votar
