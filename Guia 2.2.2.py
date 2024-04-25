75890dd6830aba5903015fd7e7e1ac5b54dd32f1
apotheosisss <cl.aro@duocuc.cl>
2024-04-25 01:10:51 -0400

    Create Guia 2.2.2.py

---
+++ Guia 2.2.2.py
@@ -0,0 +1,17 @@
+# 2.2.2
+#1
+print("Bienvenido al mundo de la programación")
+nom =input("Para comenzar, ingresa tu nombre. ")
+#2
+print(f"Bienvenido, {nom}")
+#3
+x = int(input("Ingrese el valor de x. "))
+ecuacion1 = x^2 + 3 * x + 1
+resultado = ecuacion1 / 4
+print(f"El resultado de la ecuación es: {resultado}")
+#4
+nombre = input("Bienvenido usuario, por favor ingrese su nombre.")
+rut = input("Por favor ingrese su rut.")
+correo = input("Ingrese su correo electrónico.")
+tel = input("Ingrese su número telefónico.")
+print(f"NOMBRE: {nombre} \n RUT: {rut} \n CORREO: {correo} \n TELEFONO: {tel}")
